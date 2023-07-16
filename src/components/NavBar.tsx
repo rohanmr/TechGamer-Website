51f31be52aad550689b8f2ecf8d853c7f93bb612
Rohan2104 <maindarkarrohan@yahoo.com>
2023-07-16 23:01:07 +0530

    Build a nav bar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.png";
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px" />
+      <Text>isitCity</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
